1cfc8e1762acce3c869776382afbad99f1cdd85e
Chill_guy <afanasiev.nicholas@gmail.com>
2026-05-19 19:13:29 +0200

    Edit autostart config

--- hypr/modules/autostart.lua
+++ hypr/modules/autostart.lua
@@ -1,15 +1,18 @@
--------------------
----- AUTOSTART ----
--------------------
+hl.on("hyprland.start", function ()
+  -- Wallpaper
+  hl.exec_cmd("hyprpaper -c ~/.config/.dotfiles/configs/hypr/hyprland_settinigs/hyprpaper.conf")
 
--- See https://wiki.hypr.land/Configuring/Basics/Autostart/
+  -- Bar
+  hl.exec_cmd("waybar -c ~/.config/.dotfiles/configs/hypr/waybar/config.json -s ~/.config/.dotfiles/configs/hypr/waybar/style.css")
 
--- Autostart necessary processes (like notifications daemons, status bars, etc.)
--- Or execute your favorite apps at launch like this:
---
--- hl.on("hyprland.start", function ()
---   hl.exec_cmd(terminal)
---   hl.exec_cmd("nm-applet")
---   hl.exec_cmd("waybar & hyprpaper & firefox")
--- end)
+  -- Notifications
+  hl.exec_cmd("swaync -c $HOME/.config/.dotfiles/configs/hypr/swaync/config.json -s $HOME/.config/.dotfiles/configs/hypr/swaync/style.css")
 
+  -- Clipboard history
+  hl.exec_cmd("wl-paste --type text --watch cliphist store")
+  hl.exec_cmd("wl-paste --type image --watch cliphist store")
+  hl.exec_cmd("wl-clip-persist --clipboard regular")
+
+  -- Idle daemon
+  hl.exec_cmd("hypridle -c $HOME/.config/.dotfiles/configs/hypr/hyprland_settinigs/hypridle.conf")
+end)
