391858b724dfa83652c0d0352d9a6b29abec2f16
Norexil <afanasiev.nicholas@gmail.com>
2026-08-09 22:49:55 +0200

    feat(hypr): change the autostart.lua to related waybar path

--- configs/hypr/modules/autostart.lua
+++ configs/hypr/modules/autostart.lua
@@ -1,9 +1,14 @@
+local utils = require("modules.submaps.utils")
+
+local module_dir = utils.current_dir()
+local waybar_launch = module_dir .. "../../waybar/launch.sh"
+
 hl.on("hyprland.start", function ()
   -- Wallpaper
   hl.exec_cmd("hyprpaper -c ~/.config/.dotfiles/configs/hypr/hyprland_settinigs/hyprpaper.conf")
 
   -- Bar
-  hl.exec_cmd("waybar -c ~/.config/.dotfiles/configs/hypr/waybar/config.json -s ~/.config/.dotfiles/configs/hypr/waybar/style.css")
+  hl.exec_cmd(waybar_launch)
 
   -- Notifications
   hl.exec_cmd("swaync -c $HOME/.config/.dotfiles/configs/hypr/swaync/config.json -s $HOME/.config/.dotfiles/configs/hypr/swaync/style.css")
