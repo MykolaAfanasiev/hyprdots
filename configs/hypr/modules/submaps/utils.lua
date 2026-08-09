424ba494368756e5a9b20af407cbfe0557105374
Norexil <afanasiev.nicholas@gmail.com>
2026-08-09 23:08:44 +0200

    fix(hypr): update current dir function because gnu stow

--- configs/hypr/modules/submaps/utils.lua
+++ configs/hypr/modules/submaps/utils.lua
@@ -10,10 +10,17 @@ function M.current_dir(level)
     local source = debug.getinfo(level or 2, "S").source
     local path = source:sub(1, 1) == "@" and source:sub(2) or source
 
+    local handle = assert(
+        io.popen('realpath "' .. path .. '"'),
+        "Failed to execute realpath"
+    )
+
+    local real_path = handle:read("*l")
+    handle:close()
+
     return assert(
-        path:match("(.*/)"),
+        real_path:match("(.*/)"),
         "Failed to determine current file directory"
     )
 end
-
 return M
