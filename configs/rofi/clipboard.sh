d6053dcf20f56f45db429d3f203d19a3d6f19d9e
Norexil <afanasiev.nicholas@gmail.com>
2026-08-10 18:32:39 +0200

    refactor(rofi): make cleaner code

--- configs/rofi/clipboard.sh
+++ configs/rofi/clipboard.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ROFI_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")/.." && pwd)"
+ROFI_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 
 selection="$(
     cliphist list |
@@ -8,7 +8,7 @@ selection="$(
             -dmenu \
             -display-columns 2 \
             -config "$ROFI_DIR/config.rasi" \
-            -theme "$ROFI_DIR/rofi/theme.rasi"
+            -theme "$ROFI_DIR/theme.rasi"
 )"
 
 [[ -z "$selection" ]] && exit 0
