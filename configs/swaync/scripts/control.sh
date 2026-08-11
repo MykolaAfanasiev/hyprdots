feda8fee9bc13ba5a88c089d8fbdb6578969a673
Norexil <afanasiev.nicholas@gmail.com>
2026-08-11 11:55:11 +0200

    feat(swaync): dnd notification

--- configs/swaync/scripts/control.sh
+++ configs/swaync/scripts/control.sh
@@ -1,17 +1,31 @@
 #!/usr/bin/env bash
 
+notify() {
+    hyprctl notify 5 2500 "rgb(cba6f7)" "$1"
+}
+
 case "${1:-}" in
     toggle)
         swaync-client -t
         ;;
 
     dnd)
-        swaync-client -d
+        state="$(swaync-client -d)"
+
+        if [[ "$state" == "true" ]]; then
+            notify "Do Not Disturb: ON"
+        else
+            notify "Do Not Disturb: OFF"
+        fi
         ;;
 
     reload)
-        swaync-client -R
-        swaync-client -rs
+        if swaync-client -R && swaync-client -rs; then
+            notify "SwayNC reloaded"
+        else
+            hyprctl notify 3 3500 "rgb(f38ba8)" "SwayNC reload failed"
+            exit 1
+        fi
         ;;
 
     *)
