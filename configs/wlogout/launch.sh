0af9ddf9ac3741f4c83e37de7aaec3cd1a035324
Norexil <afanasiev.nicholas@gmail.com>
2026-08-23 16:05:28 +0200

    fix(wlogout): do not open more than 1 wlogout

--- configs/wlogout/launch.sh
+++ configs/wlogout/launch.sh
@@ -2,6 +2,12 @@
 
 set -euo pipefail
 
+if pgrep -x wlogout >/dev/null; then
+    pkill -x wlogout
+    exit 0
+fi
+
+
 WLOGOUT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 
 export PATH="$WLOGOUT_DIR/scripts:$PATH"
