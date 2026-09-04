4255d7ba4e88d8cd9fcaa6677283314e27489f75
Norexil <afanasiev.nicholas@gmail.com>
2026-09-04 12:43:39 +0200

    feat(music): add MPD and rmpc setup

--- configs/zsh/modules/00-environment.zsh
+++ configs/zsh/modules/00-environment.zsh
@@ -4,12 +4,16 @@ export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
 export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
 export XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
 
+if [[ -n "${XDG_RUNTIME_DIR:-}" ]]; then
+  export MPD_HOST="$XDG_RUNTIME_DIR/mpd/socket"
+fi
+
 # Keep PATH ordered and free of duplicates
 typeset -U path PATH
 
 path=(
-    "$HOME/.local/bin"
-    "${path[@]}"
+  "$HOME/.local/bin"
+  "${path[@]}"
 )
 
 export PATH
