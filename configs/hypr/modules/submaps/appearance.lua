ecd190d26025189b323fa4beeff7a45b8893970a
Norexil <afanasiev.nicholas@gmail.com>
2026-09-10 13:03:46 +0200

    fix(hyprland): reset appearance submap before picker

--- configs/hypr/modules/submaps/appearance.lua
+++ configs/hypr/modules/submaps/appearance.lua
@@ -1,8 +1,14 @@
 -- Variables
 local vars = require("modules.vars.global")
 local submap = require("modules.submaps.utils")
-
 local paths = require("modules.vars.paths")
+
+local function launch_and_reset(command)
+  return function()
+    hl.dispatch(hl.dsp.submap("reset"))
+    hl.dispatch(hl.dsp.exec_cmd(command))
+  end
+end
 
 -- ==========================================
 -- Open appearance submap
@@ -15,9 +21,8 @@ hl.bind(vars.mainMod .. " + A", submap.switch("appearance"))
 -- ==========================================
 
 hl.define_submap("appearance", function()
-	hl.bind("W", hl.dsp.exec_cmd(paths.wallpaper_switcher.launch))
-
-	hl.bind("T", hl.dsp.exec_cmd(paths.theme_switcher.rofi))
+  hl.bind("W", launch_and_reset(paths.wallpaper_switcher.launch))
+  hl.bind("T", launch_and_reset(paths.theme_switcher.rofi))
 
   hl.bind("escape", submap.switch("reset"))
 end)
